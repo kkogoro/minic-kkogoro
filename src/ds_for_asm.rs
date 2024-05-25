aa066233559cf6ece1dbb90aacab6e79d5a61f49
夏廷轩 <2200012930@stu.pku.edu.cn>
2024-05-25 14:12:37 +0800

    lv4_done

--- src/ds_for_asm.rs
+++ src/ds_for_asm.rs
@@ -1,39 +1,149 @@
+use core::panic;
 use koopa::ir::entities::ValueData;
 use koopa::ir::Value;
-
 use std::collections::HashMap;
+use std::fs::File;
+use std::io::Write;
+
 const TMP_REG: [&str; 15] = [
     "t0", "t1", "t2", "t3", "t4", "t5", "t6", "a0", "a1", "a2", "a3", "a4", "a5", "a6", "a7",
 ];
+
+//ture if val is in range [-2048, 2047]
+pub fn check_i12(val: i32) -> bool {
+    val >= -2048 && val <= 2047
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum UserKind {
+    Val(Value),
+    Tmpi32(i32),
+}
+
 #[derive(Debug)]
 pub struct GenerateAsmInfo {
-    pub reg_user: Vec<Option<Value>>,
+    pub reg_user: Vec<Option<UserKind>>,
+    pub name_to_offset: HashMap<Value, i32>,
+    pub stack_size: i32,
 }
 
 impl GenerateAsmInfo {
     pub fn new() -> Self {
         GenerateAsmInfo {
             reg_user: vec![None; 15],
+            name_to_offset: HashMap::new(),
+            stack_size: 0,
         }
     }
-    pub fn new_reg(&mut self, value: Value) -> Option<String> {
+    ///将value的对sp偏移量设置为offset
+    pub fn set_offset(&mut self, value: Value, offset: i32) {
+        self.name_to_offset.insert(value, offset);
+    }
+    ///在栈上分配alloc
+    pub fn alloc(&mut self, output: &mut File, value: Value, offset: i32) {
+        self.set_offset(value, offset);
+    }
+    ///在栈上分配变量，并且将inst的运算结果存到栈上
+    pub fn new_var(&mut self, output: &mut File, value: Value, offset: i32) {
+        self.set_offset(value, offset);
+        let reg = self.get_reg(output, value);
+        if check_i12(offset) {
+            writeln!(output, "  sw {}, {}(sp)", reg, offset).unwrap();
+        } else {
+            let addr_reg = self.get_reg_i32(output, offset);
+            writeln!(output, "  add {}, sp, {}", addr_reg, addr_reg).unwrap();
+            writeln!(output, "  sw {}, 0({})", reg, addr_reg).unwrap();
+            self.free_reg(UserKind::Tmpi32(offset));
+        }
+    }
+    //TODO: 把寄存器当cache用，spill很好做，维护好寄存器上存没存变量，如果存了直接从寄存器读，没存从内存读，每次踢出写回内存
+    pub fn set_sp(&mut self, output: &mut File) {
+        let delta = -self.stack_size;
+        if check_i12(delta) {
+            writeln!(output, "  addi sp, sp, {}", delta).unwrap();
+            return;
+        } else {
+            let reg = self.get_reg_i32(output, delta);
+
+            writeln!(output, "  add sp, sp, {}", reg).unwrap();
+
+            self.free_reg(UserKind::Tmpi32(delta));
+        }
+    }
+    pub fn reset_sp(&mut self, output: &mut File) {
+        let delta = self.stack_size;
+        if check_i12(delta) {
+            writeln!(output, "  addi sp, sp, {}", delta).unwrap();
+            return;
+        } else {
+            let reg = self.get_reg_i32(output, delta);
+
+            writeln!(output, "  add sp, sp, {}", reg).unwrap();
+
+            self.free_reg(UserKind::Tmpi32(delta));
+        }
+    }
+    //在可用临时寄存器中分配一个寄存器
+    fn new_tmp_reg(&mut self, value: UserKind) -> String {
         for (i, user) in self.reg_user.iter_mut().enumerate() {
             if user.is_none() {
                 *user = Some(value);
-                return Some(TMP_REG[i].to_string());
+                return TMP_REG[i].to_string();
             }
         }
-        None
+        panic!("寄存器不足");
     }
-    pub fn get_reg(&mut self, value: Value) -> Option<String> {
-        for (i, user) in self.reg_user.iter().enumerate() {
-            if let Some(v) = user {
-                if *v == value {
-                    return Some(TMP_REG[i].to_string());
+    // 释放寄存器
+    pub fn free_reg(&mut self, free_user: UserKind) {
+        for iter in self.reg_user.iter_mut() {
+            if let Some(user) = iter {
+                if *user == free_user {
+                    *iter = None;
+                    return;
                 }
             }
         }
-
-        Self::new_reg(self, value)
+        panic!("user不占有任何寄存器!");
+    }
+    //专门为超过i12的偏移量分配寄存器
+    pub fn get_reg_i32(&mut self, output: &mut File, inum: i32) -> String {
+        let reg = self.new_tmp_reg(UserKind::Tmpi32(inum));
+        writeln!(output, "  li {}, {}", reg, inum).unwrap();
+        reg
+    }
+    // 获取value对应的寄存器
+    pub fn get_reg(&mut self, output: &mut File, value: Value) -> String {
+        //查询是否已经有reg
+        for (i, user) in self.reg_user.iter().enumerate() {
+            if let Some(v) = user {
+                if *v == UserKind::Val(value) {
+                    return TMP_REG[i].to_string();
+                }
+            }
+        }
+        //还在栈中，分配一个寄存器
+        let reg = Self::new_tmp_reg(self, UserKind::Val(value));
+        let opt_offset = self.name_to_offset.get(&value);
+        match opt_offset {
+            Some(offset) => {
+                //已经分配入栈，直接读出来
+                let offset = *offset;
+                if check_i12(offset) {
+                    //偏移量在i12
+                    writeln!(output, "  lw {}, {}(sp)", reg, offset).unwrap();
+                } else {
+                    //偏移量超出i12
+                    let reg_inum = self.get_reg_i32(output, offset);
+                    writeln!(output, "  add {}, sp, {}", reg_inum, reg_inum).unwrap();
+                    writeln!(output, "  lw {}, 0({})", reg, reg_inum).unwrap();
+                    self.free_reg(UserKind::Tmpi32(offset));
+                }
+                reg
+            }
+            None => {
+                //尚未分配入栈，不生成读栈代码
+                reg
+            }
+        }
     }
 }
--- src/gen_asm.rs
+++ src/gen_asm.rs
@@ -1,7 +1,9 @@
 //! 根据内存形式 Koopa IR 生成汇编
 use std::{fs::File, io::Write};
 
+use crate::ds_for_asm::check_i12;
 use crate::ds_for_asm::GenerateAsmInfo;
+use crate::ds_for_asm::UserKind;
 use koopa::ir::*;
 pub trait GenerateAsm {
     type GenerateResult;
@@ -20,11 +22,27 @@ fn get_reg(
             if val.value() == 0 {
                 return "x0".to_string();
             }
-            let reg = func_info.get_reg(value).expect("寄存器不足");
-            writeln!(output, "  li {}, {}", reg, val.value()).unwrap();
+            let reg = func_info.get_reg_i32(output, val.value());
             reg
         }
-        _ => func_info.get_reg(value).expect("寄存器不足"), //expr
+        _ => func_info.get_reg(output, value), //expr
+    }
+}
+
+///释放寄存器，TODO:先都释放掉，后面加spill再把用到这个的都删掉
+fn free_reg(func_data: &koopa::ir::FunctionData, func_info: &mut GenerateAsmInfo, value: Value) {
+    let val_data = func_data.dfg().value(value);
+    match val_data.kind() {
+        ValueKind::Integer(val) => {
+            if val.value() == 0 {
+                //0不需要释放，之前用的x0
+                return;
+            }
+            func_info.free_reg(UserKind::Tmpi32(val.value()));
+        }
+        _ => {
+            func_info.free_reg(UserKind::Val(value));
+        }
     }
 }
 
@@ -49,14 +67,46 @@ impl GenerateAsm for koopa::ir::FunctionData {
             return;
         }
 
-        let mut func_info = GenerateAsmInfo::new();
-
-        // 遍历基本块列表
         writeln!(output, "  .globl {}", &self.name()[1..]).unwrap();
         writeln!(output, "{}:", &self.name()[1..]).unwrap();
+
+        let mut func_info = GenerateAsmInfo::new();
+
+        //计算栈大小
+        for (&bb, node) in self.layout().bbs() {
+            for &inst in node.insts().keys() {
+                let value_data = self.dfg().value(inst);
+                let value_type = value_data.ty();
+                if value_type.is_unit() {
+                    //没有返回值，不需要分配栈空间
+                    continue;
+                }
+                func_info.stack_size += value_type.size() as i32;
+            }
+        }
+
+        //对齐16
+        func_info.stack_size = (func_info.stack_size + 15) & !15; //check?
+
+        //移动栈指针
+        func_info.set_sp(output);
+
+        //为每个元素分配栈偏移量
+        let mut now_stack_offset = 0;
+        for (&bb, node) in self.layout().bbs() {
+            for &inst in node.insts().keys() {
+                let value_data = self.dfg().value(inst);
+                let value_type = value_data.ty();
+                if value_type.is_unit() {
+                    //没有返回值，不需要分配栈空间
+                    continue;
+                }
+            }
+        }
+
+        // 遍历基本块列表
         for (&bb, node) in self.layout().bbs() {
             // 一些必要的处理
-            //...
             // 遍历指令列表
             for &inst in node.insts().keys() {
                 let value_data = self.dfg().value(inst);
@@ -71,19 +121,67 @@ impl GenerateAsm for koopa::ir::FunctionData {
                                 match ret_data.kind() {
                                     ValueKind::Integer(val) => {
                                         writeln!(output, "  li a0, {}", val.value()).unwrap();
-                                        writeln!(output, "  ret").unwrap();
                                     }
-                                    ValueKind::Binary(bin_inst) => {
+                                    _ => {
+                                        //其他情况**都**直接从内存读到寄存器？
                                         let reg_ret =
                                             get_reg(output, self, &mut func_info, ret_val);
                                         writeln!(output, "  mv a0, {}", reg_ret).unwrap();
-                                        writeln!(output, "  ret").unwrap();
+
+                                        free_reg(self, &mut func_info, ret_val);
                                     }
-                                    _ => {}
                                 }
                             }
-                            _ => {}
+                            None => {}
                         }
+
+                        assert!((now_stack_offset - func_info.stack_size).abs() < 16); //差值应该小于16
+                                                                                       //恢复栈指针
+                        func_info.reset_sp(output);
+                        writeln!(output, "  ret").unwrap();
+                    }
+                    ValueKind::Alloc(_) => {
+                        // 处理 alloc 指令
+
+                        func_info.alloc(output, inst, now_stack_offset);
+                        now_stack_offset += value_data.ty().size() as i32;
+                    }
+                    ValueKind::Load(load_inst) => {
+                        // 处理 load 指令
+                        let addr = load_inst.src();
+                        let offset = *func_info.name_to_offset.get(&addr).unwrap();
+                        let reg_ret = get_reg(output, self, &mut func_info, inst);
+
+                        if check_i12(offset) {
+                            writeln!(output, "  lw {}, {}(sp)", reg_ret, offset).unwrap();
+                        } else {
+                            let reg_addr = func_info.get_reg_i32(output, offset);
+                            writeln!(output, "  add {}, sp, {}", reg_addr, reg_addr).unwrap();
+                            writeln!(output, "  lw {}, 0({})", reg_ret, reg_addr).unwrap();
+                            func_info.free_reg(UserKind::Tmpi32(offset));
+                        }
+
+                        func_info.new_var(output, inst, now_stack_offset);
+                        now_stack_offset += value_data.ty().size() as i32;
+                        free_reg(self, &mut func_info, inst);
+                    }
+                    ValueKind::Store(store_inst) => {
+                        // 处理 store 指令
+                        let addr = store_inst.dest();
+                        let offset = *func_info.name_to_offset.get(&addr).unwrap();
+                        let value = store_inst.value();
+                        let reg_val = get_reg(output, self, &mut func_info, value);
+
+                        if (check_i12(offset)) {
+                            writeln!(output, "  sw {}, {}(sp)", reg_val, offset).unwrap();
+                        } else {
+                            let reg_addr = func_info.get_reg_i32(output, offset);
+                            writeln!(output, "  add {}, sp, {}", reg_addr, reg_addr).unwrap();
+                            writeln!(output, "  sw {}, 0({})", reg_val, reg_addr).unwrap();
+                            func_info.free_reg(UserKind::Tmpi32(offset));
+                        }
+
+                        free_reg(self, &mut func_info, value);
                     }
                     ValueKind::Binary(bin_inst) => {
                         // 处理二元运算
@@ -170,6 +268,15 @@ impl GenerateAsm for koopa::ir::FunctionData {
                             }
                             _ => {}
                         }
+
+                        //正式分入栈中，会在func_info生成一条sw指令
+                        func_info.new_var(output, inst, now_stack_offset);
+                        now_stack_offset += value_data.ty().size() as i32;
+
+                        //释放所有用到的寄存器(如果有)
+                        free_reg(self, &mut func_info, lhs);
+                        free_reg(self, &mut func_info, rhs);
+                        free_reg(self, &mut func_info, inst);
                     }
                     // 其他种类暂时遇不到
                     _ => {}
